58ba83780b95a6c1017a381671bb37287e2edb2d
Kate Foust <kate.e.foust@gmail.com>
2020-01-12 19:02:44 -0800

    working game

---
+++ assets/javascript/app.js
@@ -0,0 +1,67 @@
+console.log("is this working?")
+$(document).ready(function () {
+
+    //initial array of celebrities
+    var celebrities = ["Beyonce", "Kim Kardashian", "Tom Cruise", "Dwayne Johnson", "Britney Spears"];
+
+    //write function for HTML to display current content
+    function renderButtons() {
+        $("#celeb-buttons").empty();
+        for (var i = 0; i < celebrities.length; i++) {
+            var btn = `<button class="celeb" info=${i}>${celebrities[i]}</button>`
+            $("#celeb-buttons").append(btn)
+        }
+        $('.celeb').on('click', function () {
+            var index = $(this).attr("info");
+            search(celebrities[index])
+
+        })
+
+    }
+
+    function search(celebrity) {
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + celebrity + "&api_key=GbRihUv0Hb08GrTIBghIXh1vFSkQWBxt&limit=10";
+        console.log(queryURL);
+        $.ajax({
+            url: queryURL,
+            method: "Get"
+        }).done(function (response) {
+            $("#gif-output").empty();
+
+            var results = response.data;
+            for (var i=0; i<results.length; i++){
+                var img = '<img class="celeb-gif" alt-src=' + results[i].images.downsized.url  + ' src=' + results[i].images.downsized_still.url + '>'
+                $("#gif-output").append(img);
+            }
+
+            $('.celeb-gif').on('click', function(){
+                var src = $(this).attr("src")
+                var altSrc= $(this).attr("alt-src")
+                $(this).attr("src", altSrc)
+                $(this).attr("alt-src", src) 
+            })            
+
+            console.log(response);
+        })
+
+   
+    }
+
+
+    $('#add').on('click', function(){
+        event.preventDefault()
+        console.log("click add")
+        var input = $('#add-celeb').val();
+        console.log(input)
+        search(input);
+        celebrities.push(input);
+        renderButtons();
+    })
+
+    renderButtons()
+
+});
+
+//redender set of default buttons (var=celebrities)
+//build a form to gather information that user wants
+//
